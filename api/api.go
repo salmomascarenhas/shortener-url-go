66272c5d3833e086e839c9873f189825b37c9869
salmomascarenhas <salmo.cruz@gmail.com>
2024-10-08 20:08:22 -0300

    Refactor API package to handle POST and GET requests
    - Refactored the API package to include separate handler functions for handling POST and GET requests.
    - Added error handling for JSON marshaling and response writing.
    - Implemented URL validation for the POST request.
    - Generated a random code for shortening URLs.
    - Updated the handler functions to use the new code and URL mapping.

--- api/api.go
+++ api/api.go
@@ -1,26 +1,49 @@
 package api
 
 import (
+	"encoding/json"
+	"log/slog"
+	"math/rand/v2"
 	"net/http"
+	"net/url"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func NewHandler() http.Handler {
+func sendJSON(w http.ResponseWriter, resp ShortenResponse, status int) {
+	w.Header().Set("Content-Type", "application/json")
+	data, err := json.Marshal(resp)
+	if err != nil {
+		slog.Error("failed to marshal response: ", "error", err)
+		sendJSON(w, ShortenResponse{
+			Error: "something went wrong"},
+			http.StatusInternalServerError,
+		)
+		return
+	}
+
+	w.WriteHeader(status)
+	if _, err := w.Write(data); err != nil {
+		slog.Error("failed to write response to client", "error", err)
+		return
+	}
+}
+
+func NewHandler(db map[string]string) http.Handler {
 	r := chi.NewMux()
 
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
 
-	r.Post("/api/shorten", HandlePost)
-	r.Get("/{code}", HandleGet)
+	r.Post("/api/shorten", handlePost(db))
+	r.Get("/{code}", handleGet(db))
 
 	return r
 }
 
-type ShortenBoy struct {
+type ShortenBody struct {
 	URL string `json:"url"`
 }
 
@@ -29,10 +52,43 @@ type ShortenResponse struct {
 	Data  any    `json:"data,omitempty"`
 }
 
-func HandlePost(w http.ResponseWriter, r *http.Request) {
+func handlePost(db map[string]string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var body ShortenBody
+		if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+			sendJSON(w, ShortenResponse{
+				Error: "invalid request body"},
+				http.StatusBadRequest,
+			)
+			return
+		}
+
+		if _, err := url.Parse(body.URL); err != nil {
+			sendJSON(w, ShortenResponse{Error: "invalid url"}, http.StatusBadRequest)
+			return
+		}
+
+		code := genCode()
+		db[code] = body.URL
+		sendJSON(w, ShortenResponse{Data: code}, http.StatusCreated)
+	}
 
 }
 
-func HandleGet(w http.ResponseWriter, r *http.Request) {
+const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func genCode() string {
+	const n = 8
+	byts := make([]byte, n)
+	for i := range n {
+		byts[i] = characters[rand.IntN(len(characters))]
+	}
+	return string(byts)
+}
+
+func handleGet(db map[string]string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
 
 }
